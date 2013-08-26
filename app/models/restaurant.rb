762fe4df64d77917c53161c8a1580a724d9e4d5e
tracymu <tracy.musung@moomumedia.com>
2013-08-26 17:25:37 +1000

    Trying to fix bootstrap

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,10 +4,23 @@ class Restaurant < ActiveRecord::Base
 	has_many :visits
 	has_many :future_visits
 	
+	before_validation :fix_url_protocol
+
+
+
 	# def average_rating
 	# 	@restaurant.average_rating = @restaurant.visits.average(:rating).to_s
 	# 	@restaurant.save
 	# end
 
+
+	protected
+
+	def fix_url_protocol
+		unless self.website[/^http:\/\//] || self.website[/^https:\/\//]
+			self.website = 'http://' + self.website
+		end
+
+	end
 
 end
