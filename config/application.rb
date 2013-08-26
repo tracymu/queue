4b0bd315871168ced405fdeb9bb25b889f000607
tracymu <tracy.musung@moomumedia.com>
2013-08-26 18:42:26 +1000

    Fix timezone config

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
-config.time_zone = 'Sydney'
 
 module RestaurantApp
   class Application < Rails::Application
@@ -17,6 +16,7 @@ module RestaurantApp
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Sydney'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
